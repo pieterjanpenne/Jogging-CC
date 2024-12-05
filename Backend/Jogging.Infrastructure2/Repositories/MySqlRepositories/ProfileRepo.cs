e0acac5e1a19451b1ccab24ca1d434c3181464b6
zanerebouded@gmail.com <TheUnquiet>
2024-12-05 15:26:12 +0100

    Profile Repo herschreven

--- Backend/Jogging.Infrastructure2/Repositories/MySqlRepositories/ProfileRepo.cs
+++ Backend/Jogging.Infrastructure2/Repositories/MySqlRepositories/ProfileRepo.cs
@@ -3,32 +3,47 @@ using Jogging.Domain.Exceptions;
 using Jogging.Domain.Interfaces.RepositoryInterfaces;
 using Jogging.Domain.Models;
 using Jogging.Domain.Validators;
+using Jogging.Infrastructure2.Data;
+using Microsoft.EntityFrameworkCore;
 using Supabase;
-using Profile = Jogging.Infrastructure.Models.DatabaseModels.Account.Profile;
 
 namespace Jogging.Infrastructure.Repositories.SupabaseRepos;
 
 public class ProfileRepo: IProfileRepo
 {
-    private readonly Client _client;
+    private readonly JoggingCcContext _context;
     private readonly IMapper _mapper;
 
-    public ProfileRepo(Client client, IMapper mapper)
+    public ProfileRepo(JoggingCcContext context, IMapper mapper)
     {
-        _client = client;
+        _context = context;
         _mapper = mapper;
     }
     
     public async Task UpdateAsync(string userId, ProfileDom updatedItem)
     {
-        var updatedProfile = await _client.From<Profile>()
-            .Where(p => p.Id == userId)
-            .Set(p => p.Role, updatedItem.Role)
-            .Update();
+        /*
+        var profile = await _context.Profiles
+        .FirstOrDefaultAsync(p => p.Id == userId);
 
-        if (updatedProfile.Models.Count == 0)
+        if (profile == null)
         {
-            throw new ProfileException("Error while updating user role.");
+            throw new ProfileException("Profile not found.");
         }
+
+        try
+        {
+            profile.Role = updatedItem.Role;
+            _context.Profiles.Update(profile);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"UpdateAsync: {ex.Message}");
+        }
+        */
+
+        // Onzeker door die id's
+        throw new NotImplementedException();
     }
 }
